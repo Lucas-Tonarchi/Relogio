4ad26a910c8de9fd6c36e13f61bebbf683e1e0a8
Lucas-Tonarchi <abnarchi@hotmail.com>
2024-07-24 12:17:44 -0300

    Update pro1_relogio.py

--- pro1_relogio.py
+++ pro1_relogio.py
@@ -3,7 +3,7 @@ from tkinter import *
 import os
 from time import strftime
 
-#frt 
+#front 
 root = tk.Tk()
 root.title("Watch")
 root.geometry("750x400")
@@ -14,33 +14,33 @@ root.configure(background='#000000')
 
 def get_hi():
     user_name = os.getlogin()
-    hi.config(text="Hello, " + user_name)
+    hi.config(text="Olá, " + user_name)
     
-def get_dt():
-    crrt_dt = strftime("%a, %d, %b, %Y")
-    dt.config(text=crrt_dt)
-    
-def get_tm():
-    crrt_tm = strftime("%H:%M:%S")
-    tm.config(text=crrt_tm)
-    tm.after(1000, get_tm)
+def get_date():
+    current_date = strftime("%a, %d, %b, %Y")
+    date.config(text=current_date)
+     
+def get_time():
+    current_time = strftime("%H:%M:%S")
+    time.config(text=current_time)
+    time.after(1000, get_time)
 #    
 
-mrgn = tk.Canvas(root, width=750, height=100, bg="#000000", bd=0, highlightthickness=0, relief="ridge")
-mrgn.pack()
+margin = tk.Canvas(root, width=750, height=100, bg="#000000", bd=0, highlightthickness=0, relief="ridge")
+margin.pack()
 #
 
 hi = Label(root, bg="#000000", fg="#32CD32", font=("Arial", 19))
 hi.pack()
 
-dt = Label(root, bg="#000000", fg="#6B8E23", font=("Arial", 17))
-dt.pack(pady=2)
+date = Label(root, bg="#000000", fg="#6B8E23", font=("Arial", 17))
+date.pack(pady=2)
 
-tm = Label(root, bg="#000000", fg="#006400", font=("Arial", 70, "bold"))
-tm.pack(pady=2)
+time = Label(root, bg="#000000", fg="#006400", font=("Arial", 70, "bold"))
+time.pack(pady=2)
 
 get_hi()
-get_dt()
-get_tm()
+get_date()
+get_time()
 
 root.mainloop()
